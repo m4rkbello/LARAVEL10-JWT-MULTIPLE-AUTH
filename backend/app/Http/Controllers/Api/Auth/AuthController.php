3c8942990528a56b678b12ccbe7fe081faa4758c
m4rkbello <markbello10041999@gmail.com>
2024-03-02 01:26:00 +0800

    ERROR

--- backend/app/Http/Controllers/Api/Auth/AuthController.php
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -14,8 +16,28 @@ class AuthController extends Controller
     }
 
     //REGISTRATION METHOD 
-    public function registration()
+    public function registration(RegistrationRequest $request)
     {
+        $user = User::create($request->validated());
+        if($user){
+            $token = auth()->login($user);
+            return $this->responseWithToken($token, $user);
+        }else {
+            return response()->json([
+                "status" => "failed!",
+                "message" => "An error occur while trying to create user!"
+            ]);
+        }
 
+    }
+
+    //return JWT access token
+    public function responseWithToken($token, $user){
+        return response()->json([
+            "status" => "success",
+            "user" => $user,
+            "access_token" => $token,
+            "type" => 'bearer'
+        ]);
     }
 }
